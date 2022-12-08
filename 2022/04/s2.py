5ee8748863954762d1911e014c1aa031a943d73d
Lycea <20510874+Lycea@users.noreply.github.com>
2022-12-08 19:13:22 +0100

    added new days (but not uploaded)

---
+++ 2022/04/s2.py
@@ -0,0 +1,55 @@
+from helper.helpers import  *
+
+input=""
+
+class clean_set():
+    def __init__(self, set_range) -> None:
+        self._input = set_range
+        self.set_length = 0
+        self.set_start = 0
+        self.set_end = 0
+        self.stringifyed =  ""
+
+        splitted_nums = self._input.split("-")
+        self.set_end = int(splitted_nums[1])
+        self.set_start = int(splitted_nums[0])
+        self.set_length = self.set_end -self.set_start
+        self.set_range = list(range(self.set_start,self.set_end+1))
+
+        self.inside = False
+        #print(self.set_range)
+
+    def __str__(self):
+        return f"{self.set_length}"
+        
+
+    def contains(self,set_b):
+        intersections = set(self.set_range).intersection(set(set_b.set_range))
+        print(f"found {len(intersections)} clashes")
+        return len(intersections) > 0   
+            
+
+def parse():
+    global input
+    input=Input(True).split_by(",").get()
+    print(input)
+    
+
+def do():
+    parse() 
+    found_containments = 0
+    for clean_sets in input:
+        pair_sets = []
+        for set_ in clean_sets:
+            pair_sets.append(clean_set(set_))
+        
+        if pair_sets[0].contains(pair_sets[1]):
+            found_containments+=1
+            continue
+            
+    
+    print("Partial overlaps:",found_containments)
+
+def run():  
+    print("\n\ns2")
+    do()
